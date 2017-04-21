cafc3ad0c0f4c773129da7e9e9b0bfb29847b4cb
valiktorus <valiktorus@mail.ru>
2017-04-21 16:59:56 +0300

    1

--- src/by/gsu/epamlab/LineCorrector.java
+++ src/by/gsu/epamlab/LineCorrector.java
@@ -3,10 +3,8 @@ package by.gsu.epamlab;
 public abstract class LineCorrector {
 
   public static String correctLine(String line){
-
       String finalLine = RubleCorrector.correctLine(line);
       finalLine = DateCorrector.correctLine(finalLine);
-
       return finalLine;
   }
 }
