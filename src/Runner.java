b45408725b1d588c3de3a3e4419a07b94f5cf004
valiktorus <valiktorus@mail.ru>
2017-04-21 14:13:23 +0300

    1

--- src/Runner.java
+++ src/Runner.java
@@ -1,25 +1,39 @@
 import by.gsu.epamlab.LineCorrector;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
         Scanner scanner = null;
+        BufferedWriter bufferedWriter = null;
         try {
             scanner = new Scanner(new FileReader("src/in.txt"));
+            bufferedWriter = new BufferedWriter(new FileWriter("src/out.txt"));
             String inputLine;
             String outputLine;
+            String lineDelimiter = "";
             while (scanner.hasNextLine()) {
                 inputLine = scanner.nextLine();
+                outputLine = lineDelimiter + LineCorrector.correctLine(inputLine);
+                lineDelimiter = "\n";
+                bufferedWriter.write(outputLine);
                 System.out.println(LineCorrector.correctLine(inputLine));
             }
-        }catch (FileNotFoundException e){
+        }catch (FileNotFoundException e) {
             System.err.println("Input file not found");
+        }catch (IOException e) {
+            e.printStackTrace();
         }finally {
             if (scanner != null){
                 scanner.close();
+            }
+            if (bufferedWriter != null){
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
